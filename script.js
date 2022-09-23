61ff12e215b416f4e3b8cb57df4532a9f5649f13
bbluechip <cs.orhan.berk@gmail.com>
2022-09-24 00:33:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), { origin: "cors" });
+  const respData = await resp.json();
+
+  addWeatherToPage(respData);
+}
+
+function addWeatherToPage(data) {
+  const temp = KtoC(data.main.temp);
+
+  const weather = document.createElement("div");
+  console.log(data.weather[0]);
+  weather.classList.add("weather");
+  if (data.weather[0].main === "Clear") {
+    weather.innerHTML = `
+    <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+    <small>Clear Sky</small>
+`;
+    document.querySelector(
+      "body"
+    ).style.backgroundImage = `url("./img/load.webp")`;
+  }
+  if (data.weather[0].main === "Drizzle") {
+    weather.innerHTML = `
+    <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+    <small>Rainy Sky</small>
+`;
+    document.querySelector(
+      "body"
+    ).style.backgroundImage = `url("./img/rain.webp")`;
+  }
+  if (data.weather[0].main === "Clouds") {
+    weather.innerHTML = `
+    <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+    <small>Cloudy Sky</small>
+`;
+    document.querySelector(
+      "body"
+    ).style.backgroundImage = `url("./img/cloudy.jpg")`;
+  } else {
+    weather.innerHTML = `
+    <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+    <small>${data.weather[0].main}</small>
+`;
+    document.querySelector(
+      "body"
+    ).style.backgroundImage = `url("./img/load.webp")`;
+  }
+
+  // cleanup
+  main.innerHTML = "";
+
+  main.appendChild(weather);
+}
+
+function KtoC(K) {
+  return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+
+  if (city) {
+    getWeatherByLocation(city);
+  }
+});
